df2f8d7e45bf2eda683cd387fcc90d36952358f8
Israel Cohen <josh50170@outlook.co.il>
2020-10-19 15:05:37 +0300

    initial code+welcom0170

---
+++ dotNet5781_0170_5563/dotNet5781_00_0170_5563/Program0170.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_0170_5563
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome0170();
+            welcome5563();
+            Console.ReadKey();
+        }
+        static partial void welcome5563();
+        private static void welcome0170()
+        {
+            Console.Write("Enter your name: ");
+            String name0 = Console.ReadLine();
+            Console.WriteLine("{ name0 }, welcome to my first console application");
+        }
+    }
+}
