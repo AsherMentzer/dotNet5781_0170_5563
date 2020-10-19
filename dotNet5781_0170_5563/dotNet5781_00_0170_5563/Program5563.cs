73fbbf9e48b5847fc20969370e3246fda391e747
אשר מנצר <mentzer@g.jct.ac.il>
2020-10-19 15:40:41 +0300

    inial calss program5563

---
+++ dotNet5781_0170_5563/dotNet5781_00_0170_5563/Program5563.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_0170_5563
+{
+    partial class Program
+    {
+        static partial void welcome5563()
+        {
+            Console.WriteLine("hi");
+        }
+    }
+}
