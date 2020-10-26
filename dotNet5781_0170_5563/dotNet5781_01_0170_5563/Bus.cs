a975f7f29f34bfeadf04376df4978b1234e6ee9e
Israel Cohen <josh50170@outlook.co.il>
2020-10-26 18:12:44 +0200

    bulid bus class

---
+++ dotNet5781_0170_5563/dotNet5781_01_0170_5563/Bus.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bus
+{
+    public class Bus
+    {
+        public string Id;
+        DateTime ActiveDate;
+        public int Kilometrage;
+        bool dangerous;
+        int fuel;
+        int kmAfterBusFixing;
+        DateTime lastFix;
+        
+        public Bus(string id, int year, int month, int day)
+        {
+            Id = id;
+            ActiveDate = new DateTime(year, month, day);
+            lastFix = new DateTime(year, month, day);
+        }
+    }
+
+    //class Date
+    //{
+    //    int Year;
+    //    int Month;
+    //    int Day;
+
+    //    public Date(int y, int m, int d)
+    //    {
+    //        Year = y;
+    //        Month = m;
+    //        Day = d;
+    //    }
+    //}
+}
---
+++ dotNet5781_0170_5563/dotNet5781_01_0170_5563/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_0170_5563
+{
+    using Bus;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bus b1(hello, 1990, 1, 6);
+        }
+    }
+}
